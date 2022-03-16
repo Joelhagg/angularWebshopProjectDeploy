eb156cf910b895adc4ad1d68e32a9a416a335175
joel.pettersson90@gmail.com <joel.pettersson90@gmail.com>
2022-03-16 10:18:27 +0100

    changed enviroment to .prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  getFromApiURL: 'https://medieinstitutet-wie-products.azurewebsites.net/api/products',
+  postToApiUrl: 'https://medieinstitutet-wie-products.azurewebsites.net/api/orders',
+  getOrdersToAdminUrl: 'https://medieinstitutet-wie-products.azurewebsites.net/api/orders?companyId=19',
+  deleteOrderFromAdmin1: 'https://medieinstitutet-wie-products.azurewebsites.net/api/orders/',
+  deleteOrderFromAdmin2: '?companyId=19'
 };
